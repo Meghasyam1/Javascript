88577d939e27fa9af6dbfca8ee06042747b10646
Meghasyam1 <nmeghasyam1@gmail.com>
2022-05-11 12:25:31 +0530

    Javascript first program

---
+++ new_fi.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+window.alert("world");
+const obj = {
+    name : "c class",
+    brand : "Mercedes",
+    log : function() {
+        console.log("Inside log function");
+    }
+}
+obj.log();
+console.log(obj.name);
