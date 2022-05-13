de7127a48a16d673a4b2748e391fd67d3896122c
Meghasyam1 <nmeghasyam1@gmail.com>
2022-05-13 15:16:47 +0530

    Timer class-26

--- new_fi.js
+++ new_fi.js
@@ -26,5 +26,34 @@ obj.addi();
 console.log("Name is :",obj1.name,"\n","Studying :",obj1.studying,"\n","college :",obj1.college);
 console.log(obj.name);
 function myfunction(){
-document.getElementById("demo").innerHTML="Paragraph changed"
+document.getElementById("demo").innerHTML="Text changed";
 }
+val = 0;
+function increment() {
+    val++;
+    document.getElementById("num").innerHTML=val
+}
+function decrement() {
+    val--;
+    document.getElementById("num").innerHTML=val
+}
+const myInterval=setInterval(run,1000);
+sval = 0;
+ispause = true;
+function run(){
+    if(!ispause){
+    sval++;
+    document.getElementById("sno").innerText=sval;
+    }
+}
+function start(){
+    ispause = false;
+}
+function stop(){
+    ispause=true;
+}
+function reset(){
+    sval=0;
+    document.getElementById("sno").innerText=sval;
+}
+
